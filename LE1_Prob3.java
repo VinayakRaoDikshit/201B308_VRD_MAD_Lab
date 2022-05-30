cad427d124dac78a7258c040933df5e42e0f0e66
VinayakRaoDikshit <99906095+VinayakRaoDikshit@users.noreply.github.com>
2022-05-30 18:30:43 +0530

    Update LE1_Prob3.java

--- LE1_Prob3.java
+++ LE1_Prob3.java
@@ -1,15 +1,7 @@
-public class LE1_Prob3
+public class Main
 {
 	public static void main(String[] args) {
-	   int rows = Integer.parseInt(args[0]);
-	   for (int i = 1 ; i <= rows ; i++){
-	       for ( int j = 1 ; j <= i ; j++)
-	        System.out.print(" ");
-	        
-	        for ( int k = rows ; k >=i ; k-- )
-	        System.out.print(" *");
-	        
-	        System.out.println();
-	   }
+	   for(int i=1; i<=10; i++)
+	        System.out.println(args[0]+"*"+i+"="+(Integer.parseInt(args[0]))*i);
 	}
 }
